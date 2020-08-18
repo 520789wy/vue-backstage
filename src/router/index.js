69b85b3e2aa9765737d40e470b72454c86adf66e
520789wy <1234656754@qq.com>
2020-08-18 22:58:14 +0800

    用户权限列表

--- src/router/index.js
+++ src/router/index.js
@@ -4,30 +4,23 @@ import Login from "../components/Login.vue";
 import Home from "../components/Home.vue";
 import Welcome from "../components/Welcome.vue";
 import User from "../components/User/User.vue";
+import Rights from "../components/power/Rights.vue";
+import Roles from "../components/power/Roles.vue";
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path:"/",
-    redirect: "/login"//路由重定向，自动跳转到登录页面
-  },
-  {
-    path:"/login",
-    component:Login
-  },
+  //路由重定向，自动跳转到登录页面
+  {path:"/",redirect: "/login"},
+  {path:"/login",component:Login},
   {
     path:"/home",
     component:Home,
     redirect:"/welcome",
     children:[
-      {
-        path:"/welcome",
-        component:Welcome
-      },
-      {
-        path:"/users",
-        component:User
-      }
+      {path:"/welcome",component:Welcome},
+      {path:"/users",component:User},
+      {path:"/rights",component:Rights},
+      {path:"/roles",component:Roles}
     ]
   }
 ];
